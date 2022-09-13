d647033b5b98b255332c67c9fe37ada2f990557b
Alliquison Matias <allyquison.matias@gmail.com>
2022-09-13 16:34:32 -0300

    Add files via upload

--- Introdução_POO/Veiculo2/Motor.cpp
+++ Introdução_POO/Veiculo2/Motor.cpp
@@ -1,24 +1,9 @@
-#include <iostream>
+#include "Motor.h"
 
-using namespace std;
-
-class Motor{
-    private:
-        int cilindros;
-        int potencia;
-    public:
-        Motor();
-        void Motor::inicializa(int cilindros, int potencia);
-
-        int getCilindros();
-        void setCilindros(int c);
-
-        int getPotencia();
-        void setPotencia(int p);
-
-        void print();
-
-};
+Motor::Motor(int c, int p){
+    cilindros=c;
+    potencia=p;
+}
 
 void Motor::print(){
     cout << "Numero de cilindros: " << cilindros << "\n";
