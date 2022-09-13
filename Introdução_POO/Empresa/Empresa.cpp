97d0b5b42b21121a2e672dea364c1f33427e3b47
Alliquison Matias <allyquison.matias@gmail.com>
2022-09-13 16:36:34 -0300

    Add files via upload

--- Introdução_POO/Empresa/Empresa.cpp
+++ Introdução_POO/Empresa/Empresa.cpp
@@ -1,39 +1,4 @@
-#include <iostream>
-
-using namespace std;
-
-class Empresa{
-    private:
-        string nome;
-        string endereco;
-        string cidade;
-        string estado;
-        string cep;
-        string telefone;
-    public:
-        Empresa(){}
-        Empresa(string nome, string endereco, string cidade, string estado, string cep, string telefone); 
-
-        string getNome();
-        void setNome(string n);
-
-        string getEndereco();
-        void setEndereco(string e);
-
-        string getCidade();
-        void setCidade(string c);
-
-        string getEstado();
-        void setEstado(string es);
-
-        string getCep();
-        void setCep(string ce);
-
-        string getTelefone();
-        void setTelefone(string t);
-
-        void print();
-};
+#include "Empresa.h"
 
 Empresa::Empresa(string n, string e, string c, string es, string ce, string t){
     nome=n;
