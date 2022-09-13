334d18fd3d2da8f1e92a71acd1ae76eb306251c7
Alliquison Matias <allyquison.matias@gmail.com>
2022-09-13 15:02:58 -0300

    Create main.cpp

---
+++ Introdução_POO/Empresa/main.cpp
@@ -0,0 +1,7 @@
+#include "Restaurante.cpp"
+
+int main(){
+    Restaurante MeuRest{"Google", "Bairro alto sao francisco", "Quixada", "Ceara", "9009087979", "(88) 9 9971-4932", "Italiana", 150};
+    MeuRest.print();
+
+};
