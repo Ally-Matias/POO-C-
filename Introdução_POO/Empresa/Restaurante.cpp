e681db6c75d0b2bfa8cb815221e77aa3741224cf
Alliquison Matias <allyquison.matias@gmail.com>
2022-09-13 16:37:08 -0300

    Add files via upload

--- Introdução_POO/Empresa/Restaurante.cpp
+++ Introdução_POO/Empresa/Restaurante.cpp
@@ -1,26 +1,4 @@
-#include "Empresa.cpp"
-
-using namespace std;
-
-class Restaurante:public Empresa{
-    private:
-        string TipComida;
-        int PrecMedio;
-    public:
-        Restaurante(string n, string e, string c, string es, string ce, string t, string tc, int pm)
-        :Empresa{n ,e ,c ,es ,ce ,t}{
-            TipComida=tc;
-            PrecMedio=pm;
-        }
-        string getTipComida();
-        void setTipComida(string tc);
-
-        int getPrecMedio();
-        void setPrecMedio(int pm);
-
-        void print();
-
-};
+#include "Restaurante.h"
 
 void Restaurante::print(){
     Empresa::print();
